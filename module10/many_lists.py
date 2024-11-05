63a00a181005cda8ffb9f6c57360cb59d68e8ac2
Hugo Valle <hugovalle1@weber.edu>
2024-11-05 19:02:33 +0000

    Refactor code

--- module10/many_lists.py
+++ module10/many_lists.py
@@ -18,27 +18,28 @@ def add_students(students, number):
     return students
 
 
-def create_student_record(student_id, scores):
+def add_student_id(student_id):
     student_info = {}  # student info, 'id' and 'scores'
     student_info['id'] = student_id  # add student ID to dictionary
+    return student_info
+
+
+def add_quiz_scores(student_info, scores):
     for number in range(1, scores + 1):
         quiz_id = f'quiz{number}'
         # Random quiz scores as values between 50 and 100 based on scores input
         student_info[quiz_id] = random.randint(50, 100)
-    return student_info
 
 
-def add_quiz_scores(students, scores):
+def build_student_info(students, scores):
     student_records = []  # return list
     # Create a dictionary with student 'id' as the key and
     # IDs from students as values
     for student_id in students:
-        student_info = create_student_record(student_id, scores)
+        student_info = add_student_id(student_id)  # returns dictionary
+        add_quiz_scores(student_info, scores)  # returns nothing
         student_records.append(student_info)  # add dictionary to list
     return student_records
-
-# Starting Point
-# Program "Driver"
 
 
 def main():
@@ -50,8 +51,11 @@ def main():
     students = add_students(students, student_number)
     print(f'2) Students: {students}')
     quiz_number = 3
-    student_grades = add_quiz_scores(students, quiz_number)
+    student_grades = build_student_info(students, quiz_number)
     pp(f'3) Student Grades: {student_grades}')
+    # quiz_number = 1
+    # student_grades = build_student_info(students, quiz_number)
+    # pp(f'4) Student Grades: {student_grades}')
 
 
 if __name__ == '__main__':
