996221014b7396c81342f09c6c415927f69f1983
Hugo Valle <hugovalle1@weber.edu>
2024-11-12 19:00:47 +0000

    Add validation for constructor

--- module11/airtravel.py
+++ module11/airtravel.py
@@ -8,7 +8,23 @@ class Flight:
     # This is called when an object is created
     def __init__(self, number):
         # Define instance variables
-        self._number = number
+        self._number = self._validate_flight_number(number)
+    
+    def _validate_flight_number(self, number):
+        # If any of the rules below fail, raise ValueError
+        # Must be 5 characters long
+        if len(number) != 5:
+            raise ValueError(f'Invalid flight number length [{number}]')
+        # First two characters (Airline Code) should be alpha
+        if not number[:2].isalpha():
+            raise ValueError(f'Invalid airline code [{number[:2]}]')
+        # First two characters should be uppercase
+        if not number[:2].isupper():
+            raise ValueError(f'Invalid airline code [{number[:2]}]')
+        # Last three characters (Airline Flight Number) should be digits
+        if not number[2:].isdigit():
+            raise ValueError(f'Invalid flight number [{number[2:]}]')
+        return number
     
     # Functions inside classes are called methods
     # This is also called a "getter" method
@@ -19,7 +35,7 @@ class Flight:
 
     # This is also called a "setter" method
     def set_number(self, number):
-        self._number = number   
+        self._number = self._validate_flight_number(number)
 
 
 def main():
--- module11/use_airtravel.py
+++ module11/use_airtravel.py
@@ -9,10 +9,22 @@ def main():
     f1 = Flight('SN060') # use parentheses to call the constructor
     print(f'Flight type {f1}')
     print(f'Flight number {f1.number()}')
+    
     f2 = Flight('DL123') # use parentheses to call the constructor
     print(f'Flight number {f2.number()}')
-    f2.set_number('DL456')
+    f2.set_number('456DL')
     print(f'Flight number {f2.number()}')
+    
+    # f3 = Flight('DL12344') # use parentheses to call the constructor
+    # print(f'Flight number {f3.number()}')
+    # f3 = Flight('9L123') # use parentheses to call the constructor
+    # print(f'Flight number {f3.number()}')
+    # f3 = Flight('dl123') # use parentheses to call the constructor
+    # print(f'Flight number {f3.number()}')
+    # f3 = Flight('DL1A3') # use parentheses to call the constructor
+    # print(f'Flight number {f3.number()}')
+    f3 = Flight('DL193') # use parentheses to call the constructor
+    print(f'Flight number {f3.number()}')
 
 
 if __name__ == '__main__':
